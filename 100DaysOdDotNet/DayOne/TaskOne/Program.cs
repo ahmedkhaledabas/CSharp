90c2b0ae0c87d4452943e560f32c9d40be3db61b
Ahmed Khaled <102932989+ahmedkhaledabas@users.noreply.github.com>
2024-02-14 05:07:30 +0200

    DayOne

---
+++ 100DaysOdDotNet/DayOne/TaskOne/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace TaskOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = MakeArray();
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(i == 0) Console.Write("Your Array : \n{ ");
+                if(i == arr.Length - 1)
+                {
+                    Console.Write(arr[i]);
+                    Console.Write(" }\n");
+                }
+                else
+                {
+                    Console.Write(arr[i]);
+                    Console.Write(" , ");
+                }
+            }
+            Console.WriteLine($"Maximun Value : {GetMax(arr)} \nMinimum Value : {GetMin(arr)} \nAvarage :{GetAvg(arr)}");
+        }
+
+        public static int[] MakeArray()
+        {
+            Console.WriteLine("Enter Size Of Array");
+            int size = Convert.ToInt16(Console.ReadLine());
+            int[] ints = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Enter Elements Number {i + 1}");
+                ints[i] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            return ints;
+        }
+
+        public static int GetMax(int[] arr)
+        {
+            Array.Sort(arr);
+            return arr[arr.Length - 1];
+        }
+
+        public static int GetMin(int[] arr)
+        {
+            Array.Sort(arr);
+            return arr[0];
+        }
+
+        public static double GetAvg(int[] arr)
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+
+            return sum / arr.Length;
+        }
+
+    }
+}
---
+++ 100DaysOdDotNet/DayOne/TaskTow/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace TaskTow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           while(true)
+            {
+                Console.WriteLine("Enter String You Want Check");
+                var s = Convert.ToString(Console.ReadLine());
+                Console.WriteLine(CheckPalindrome(s) ? "Is Palindrome" : "Isn't Palindrome");
+            }
+        }
+
+        public static bool CheckPalindrome(string palindrome)
+        {
+            int i = 0;
+            int j = palindrome.Length - 1;
+            while(i != j && i < j)
+            {
+                if (palindrome[i] != palindrome[j]) return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+    }
+}
